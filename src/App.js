4ef25a385605b8727568af8966c5fd0d133df846
Zihao <zihao-lee@outlook.com>
2023-03-03 23:59:15 +0800

    Theme with light and dark modes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
+  const [theme, colorMode] = useMode();
+  console.log(colorMode);
   return (
     <div className="app">
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRounter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRounter>
+    <BrowserRouter>
       <App />
-    </BrowserRounter>
+    </BrowserRouter>
   </React.StrictMode>
 );
