1776bfc6926aec8a6368415c32734ba2f50f7f08
Zihao <zihao-lee@outlook.com>
2023-03-07 23:48:51 +0800

    TopBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
 
 function App() {
   const [theme, colorMode] = useMode();
-  console.log(colorMode);
-  return (
-    <div className="app">
 
-    </div>
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar/>
+          </main>
+        </div>
+      </ThemeProvider>      
+    </ColorModeContext.Provider>
   );
 }
 
